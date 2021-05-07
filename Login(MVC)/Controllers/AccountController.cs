c76c2508ccd1d9c2f669f3331ac17b78ec9a78f4
Weslei Correa <wcorrea@onlineapp.com.br>
2021-05-07 12:25:48 -0300

    Primeiro Projeto

---
+++ Login(MVC)/Controllers/AccountController.cs
@@ -0,0 +1,54 @@
+﻿using Login_MVC_.Models;
+using Login_MVC_.Repositorios;
+using System;
+using System.Web.Mvc;
+
+namespace Login_MVC_.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly AccountRepository _accountRepository;
+        private bool isLogged { get { return Session != null && (Session["logged"] != null && (bool)Session["logged"] == true); } }
+
+        public AccountController()
+        {
+            _accountRepository = new AccountRepository();
+        }
+
+        // GET: Account
+        [HttpGet]
+        public ActionResult Login()
+        {
+            if (isLogged)
+                return RedirectToAction("Create");
+
+            return View("Login");
+        }
+
+        [HttpGet]
+        public ActionResult Error()
+        {
+            return View("Error");
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            if (!isLogged)
+                return RedirectToAction("Error");
+
+            return View("Create");
+        }
+
+        public ActionResult Verify(Account acc)
+        {
+            var result = _accountRepository.Verify(acc);
+            if (result == null)
+                return RedirectToAction("Error");
+
+            Session["logged"] = true;
+
+            return RedirectToAction("Create");
+        }
+    }
+}
---
+++ Login(MVC)/Repositorios/AccountRepository.cs
@@ -0,0 +1,29 @@
+﻿using Dapper;
+using Login_MVC_.Models;
+using System.Data.SqlClient;
+
+namespace Login_MVC_.Repositorios
+{
+    public class AccountRepository
+    {
+        private readonly string _connectionString;
+
+        public AccountRepository()
+        {
+            _connectionString = "Server=WESLEI04;Database=BDD_Primeiro_Login;Trusted_Connection=True;";
+        }
+
+        public Account Verify(Account account)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var result = connection.QueryFirstOrDefault<Account>(@"SELECT Name, Password 
+                                                                FROM UserName 
+                                                               WHERE Name = @Name 
+                                                                AND Password = @Password", param: account);
+
+                return result;
+            }
+        }
+    }
+}
